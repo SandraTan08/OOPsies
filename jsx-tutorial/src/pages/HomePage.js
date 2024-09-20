6cbf4c6da7368def4697b07b1a89915ce2ed9070
Amoussee <114078162+Amoussee@users.noreply.github.com>
2024-09-20 10:21:08 +0800

    next.js

--- jsx-tutorial/src/pages/HomePage.js
+++ jsx-tutorial/src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Button } from "@shadcn/ui";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+    <div className="text-3xl font-bold underline">
+      Hello, Tailwind CSS!
+      <Button className="bg-blue-500 text-white">ShadCN Button</Button>
     </div>
+
+    
   );
 };
 
