09c582d334cdf3d3a5a1b82ce9c77657706e1159
Amoussee <114078162+Amoussee@users.noreply.github.com>
2024-10-22 21:59:16 +0800

    this is for cr

--- Backend/timperio.crm/src/main/java/oopsies/timperio/crm/Newsletter.java
+++ Backend/timperio.crm/src/main/java/oopsies/timperio/crm/Newsletter.java
@@ -19,11 +19,35 @@ public class Newsletter {
     @Column(name = "newsletterId")
     private Long newsletterId;
 
+    public Long getNewsletterId() {
+        return newsletterId;
+    }
+
+    public void setNewsletterId(Long newsletterId) {
+        this.newsletterId = newsletterId;
+    }
+
     @Column(name = "accountId")
     private String accountId;  // Store the ID of the user who created this template
 
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
     @Column(name = "customerName")
     private String customerName;
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 
     // One-to-many relationship to ProductTemplate
     @OneToMany(mappedBy = "newsletter", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
