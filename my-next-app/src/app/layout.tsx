5455dc1e3c8987d1ebe8c95eb3c9d3a224187fe5
Amoussee <114078162+Amoussee@users.noreply.github.com>
2024-11-21 00:52:00 +0800

    favicon

--- my-next-app/src/app/layout.tsx
+++ my-next-app/src/app/layout.tsx
@@ -1,12 +1,16 @@
 // src/app/layout.tsx
 "use client"
 
+import Head from 'next/head';
 import './globals.css'
 import { SessionProvider } from 'next-auth/react';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <SessionProvider>{children}</SessionProvider>
       </body>
